1bd14fd616a1c7e00a3896fdd89ab1dff0f485c4
Rocky Dunlap <rocky.dunlap@noaa.gov>
2021-04-28 17:16:40 -0600

    Bring over CircleCI from previous repo

---
+++ .circleci/runscp.sh
@@ -0,0 +1,174 @@
+#!/bin/bash 
+
+set -x 
+cd ~/esmf-test-artifacts
+#echo "Host *" > ~/.ssh/config
+#echo "  StrictHostKeyChecking no" >> ~/.ssh/config
+#git clone git@github.com:mark-a-potts/esmf-test-artifacts.git
+git config --global user.email "mark.potts@noaa.gov"
+git config --global user.name "dcv-bot"
+git branch
+export hash=${CIRCLE_SHA1}
+export TERM=xterm
+export host=${CIRCLE_BRANCH}
+if [[ $host == "main" ]]; then
+  exit 0 
+fi
+export message=`git log --format=%B -n 1 $hash | head -n 1`
+export message=$message" [ci skip]"
+#export host=`echo $message |  awk -F ' ' '{print $10}'`
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m $message
+  git push origin main
+done
+
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m $message
+  git push origin main
+done
+
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m $message
+  git push origin main
+done
+
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m $message
+  git push origin main
+done
+
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m $message
+  git push origin main
+done
+
+export branch=`echo $message |  awk -F ' ' '{print $5}' | awk -F '_._' '{print $2}'`
+export bmessage=`echo $message |  awk -F ' ' '{print $8}'` 
+export branch=${branch:-'develop'}
+export branchhash=`echo $message |  awk -F ' ' '{print $8}' | awk -F '-' '{print $3}'`
+echo $hash
+echo $host
+echo $branch
+echo $message
+which column
+git checkout $hash $branch/$host
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "unit test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > unit
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "system test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > sys
+find $branch -iname summary.dat | xargs grep -l "$branchhash" | xargs grep "example test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $12,$14}' > examp
+echo "host compilier version mpi-type mpi-ver O/g unit-pass unit-fail sys-pass sys-fail ex-pass ex-fail" > $branch/$bmessage.summary
+paste -d " " unit sys examp >> "$branch/$bmessage.summary"
+git add $branch
+git pull -X theirs --no-edit origin main
+git commit -a -m"$message"
+git push origin main
+while [ $? -ne 0 ]
+do
+  git pull -X theirs --no-edit origin main
+  git commit --amend -m'$message'
+  git push origin main
+done
