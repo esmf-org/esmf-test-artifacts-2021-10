1b985890a4001b3d9c4ddc0602918ea32191cbd5
Mark Potts <Mark.Potts@noaa.gov>
2021-04-29 08:05:52 -0400

    Update from hera [ci skip]

---
+++ develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/build-gfortran_9.2.0b_intelmpi_g.bat
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o build-gfortran_9.2.0b_intelmpi_g.bat_%j.o
+#SBATCH -e build-gfortran_9.2.0b_intelmpi_g.bat_%j.e
+#SBATCH --time=1:20:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
+module load gnu/9.2.0 impi/2020 
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Mark.Potts/gfortran_9.2.0b_intelmpi_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 clean 2>&1| tee clean_$JOBID.log 
+make -j 40 2>&1| tee build_$JOBID.log
+
