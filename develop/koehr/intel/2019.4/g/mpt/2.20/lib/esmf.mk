f3e83949c5e282d7827a160aeb1ce754a470c607
Mark Potts <Mark.Potts@noaa.gov>
2021-05-01 07:45:16 -0400

    update for test of intel_2020.2_intelmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_06-2-g7cd9acb on koehr [ci skip] [ci skip]

--- develop/koehr/intel/2019.4/g/mpt/2.20/lib/esmf.mk
+++ develop/koehr/intel/2019.4/g/mpt/2.20/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-04-30 10:59:54
+build time -- 2021-05-01 08:42:57
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/koehr/intel/2020.2/O/intelmpi/2020.2.254/lib/esmf.mk
+++ develop/koehr/intel/2020.2/O/intelmpi/2020.2.254/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-04-30 10:50:57
+build time -- 2021-05-01 08:48:42
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
