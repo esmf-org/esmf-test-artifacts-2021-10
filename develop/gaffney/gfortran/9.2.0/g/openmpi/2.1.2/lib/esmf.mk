983359019f87e599ab3a38ebabfeac679f97a266
Mark Potts <Mark.Potts@noaa.gov>
2021-05-01 02:30:08 -0400

    update for test of intel_2018.1_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_06-2-g7cd9acb on gaffney [ci skip] [ci skip]

--- develop/gaffney/gfortran/9.2.0/g/openmpi/2.1.2/lib/esmf.mk
+++ develop/gaffney/gfortran/9.2.0/g/openmpi/2.1.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-04-30 06:46:43
+build time -- 2021-05-01 05:53:42
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/gaffney/intel/2018.1/g/intelmpi/18.0.1.163/lib/esmf.mk
+++ develop/gaffney/intel/2018.1/g/intelmpi/18.0.1.163/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-04-30 06:48:13
+build time -- 2021-05-01 05:54:05
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
