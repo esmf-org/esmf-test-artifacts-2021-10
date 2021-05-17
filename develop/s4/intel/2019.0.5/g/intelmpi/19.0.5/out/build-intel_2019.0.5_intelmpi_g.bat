6f13000d486e3289f2672c9da938ab3f4680c2a0
Mark Potts <mark.potts@noaa.gov>
2021-05-17 13:55:23 +0000

    update for build of intel_2019.0.5_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_07-3-g7969da3 on s4 [ci skip]

---
+++ develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/out/build-intel_2019.0.5_intelmpi_g.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=star
+#SBATCH -o build-intel_2019.0.5_intelmpi_g.bat_%j.o
+#SBATCH -e build-intel_2019.0.5_intelmpi_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=s4
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load licesnse_intel/S4 hdf
+module load intel/19.0.5 impi/19.0.5 netcdf4/4.7.3
+module load hdf5/1.10.5 
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/data/users/mpotts/intel_2019.0.5_intelmpi_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 24 clean 2>&1| tee clean_$JOBID.log 
+make -j 24 2>&1| tee build_$JOBID.log
+
