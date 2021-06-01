4475141ec30f7dfeb262fb05031bacbb6071dea8
Mark Potts <Mark.Potts@noaa.gov>
2021-06-01 03:15:08 -0400

    update for test of intel_2020_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_10-3-g469be43 on discover [ci skip]

--- develop/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/lib/esmf.mk
+++ develop/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-05-31 01:09:32.546496
+build time -- 2021-06-01 02:38:07.535021
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/discover/intel/2020/g/intelmpi/19.1.3.304/lib/esmf.mk
+++ develop/discover/intel/2020/g/intelmpi/19.1.3.304/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-05-31 01:09:22.526140
+build time -- 2021-06-01 02:38:38.232401
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
