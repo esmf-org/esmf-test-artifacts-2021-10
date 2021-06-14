305b4f21a7b9abe0ce1f2e88b03172cbe1022626
Mark Potts <Mark.Potts@noaa.gov>
2021-06-14 02:45:07 -0400

    update for test of pgi_2020_mpiuni_g_develop with hash ESMF_8_2_0_beta_snapshot_10-5-g26c4ccc on discover [ci skip]

--- develop/discover/pgi/2020/O/mpiuni/lib/esmf.mk
+++ develop/discover/pgi/2020/O/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-06-13 01:31:39.412636
+build time -- 2021-06-14 01:33:01.581134
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/discover/pgi/2020/g/mpiuni/lib/esmf.mk
+++ develop/discover/pgi/2020/g/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-06-13 01:38:55.523552
+build time -- 2021-06-14 01:41:49.401401
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
