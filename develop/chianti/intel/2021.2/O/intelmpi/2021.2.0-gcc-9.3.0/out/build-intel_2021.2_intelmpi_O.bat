fe1c55f8730ccc53753715f46029428b0f99b923
Mark Potts <Mark.Potts@noaa.gov>
2021-06-16 10:30:52 -0400

    update for test of intel_2021.2_intelmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_10-18-gc0f0685 on chianti [ci skip]

---
+++ develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+export JOBID=12345
+
+module use /home/mpotts/hpc-modules/modulefiles/stack
+
+module load hpc/1.1.0.a
+module load hpc-intel-oneapi-compilers/2021.2.0-gcc-9.3.0 hpc-intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf/4.7.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/home/mpotts/intel_2021.2_intelmpi_O_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 8 2>&1| tee build_$JOBID.log
+
