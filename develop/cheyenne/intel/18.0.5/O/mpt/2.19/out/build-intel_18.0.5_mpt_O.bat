95632003dc4991441d4ad394dc554f737b02ff56
Mark Potts <Mark.Potts@noaa.gov>
2021-06-17 08:15:09 -0400

    update for test of gfortran_10.1.0_mpt_g_develop with hash ESMF_8_2_0_beta_snapshot_11 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/mpt/2.19/out/build-intel_18.0.5_mpt_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpt/2.19/out/build-intel_18.0.5_mpt_O.bat
@@ -22,7 +22,6 @@ export ESMF_COMM=mpt
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-make -j 36 clean 2>&1| tee clean_$JOBID.log 
 make -j 36 2>&1| tee build_$JOBID.log
 
 ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_mpt_O_develop/getres-build.sh
