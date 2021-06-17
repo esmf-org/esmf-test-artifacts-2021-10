b2ec0a79c74b69efb306482ef96018526a674fbf
Mark Potts <mpotts@cgdm-catania.cgd.ucar.edu>
2021-06-17 07:35:20 -0600

    update for build of gfortran_10.2.0_mpich3_O_develop with hash ESMF_8_2_0_beta_snapshot_11 on catania [ci skip]

--- develop/catania/gfortran/10.2.0/O/mpich3/3.3.2-custom/out/build-gfortran_10.2.0_mpich3_O.bat
+++ develop/catania/gfortran/10.2.0/O/mpich3/3.3.2-custom/out/build-gfortran_10.2.0_mpich3_O.bat
@@ -16,6 +16,5 @@ export ESMF_COMM=mpich3
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-make -j 4 clean 2>&1| tee clean_$JOBID.log 
 make -j 4 2>&1| tee build_$JOBID.log
 
