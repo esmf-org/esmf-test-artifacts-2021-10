88447787c8d4330743f93bed0abc7feb59eff6d8
Mark Potts <Mark.Potts@noaa.gov>
2021-08-14 03:45:05 -0400

    update for test of gfortran_9.2.0_openmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_15 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-13 07:09:59
+build time -- 2021-08-14 06:57:43
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/intel/18.0.4/O/mpiuni/lib/esmf.mk
+++ develop/hera/intel/18.0.4/O/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-13 06:10:53
+build time -- 2021-08-14 07:02:44
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
