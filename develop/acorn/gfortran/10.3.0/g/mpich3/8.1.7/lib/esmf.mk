4ce92c9819b6218f0a956051cd0f18111e9af16b
Mark Potts <Mark.Potts@noaa.gov>
2021-08-15 13:15:06 -0400

    update for test of intel_2019.3_mpi_O_develop with hash ESMF_8_2_0_beta_snapshot_15 on acorn [ci skip]

--- develop/acorn/gfortran/10.3.0/g/mpich3/8.1.7/lib/esmf.mk
+++ develop/acorn/gfortran/10.3.0/g/mpich3/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-14 16:33:56
+build time -- 2021-08-15 16:33:50
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/acorn/intel/2019.3/g/mpi/8.1.7/lib/esmf.mk
+++ develop/acorn/intel/2019.3/g/mpi/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-14 16:38:12
+build time -- 2021-08-15 16:38:05
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
