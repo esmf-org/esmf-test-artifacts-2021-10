91bb8d2370688efc663267c27dd54b3a98d89e6c
Mark Potts <Mark.Potts@noaa.gov>
2021-08-16 07:00:04 -0400

    update for test of gfortran_8.3.0_openmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_15 on orion [ci skip]

--- develop/orion/gfortran/8.3.0/g/openmpi/4.0.2/lib/esmf.mk
+++ develop/orion/gfortran/8.3.0/g/openmpi/4.0.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-15 02:34:04
+build time -- 2021-08-16 03:30:21
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/orion/intel/2020/O/intelmpi/2020.2/lib/esmf.mk
+++ develop/orion/intel/2020/O/intelmpi/2020.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-15 02:34:02
+build time -- 2021-08-16 03:35:12
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
