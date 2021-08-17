e3797bced163157f4cb91b933ce522e34284770f
Mark Potts <Mark.Potts@noaa.gov>
2021-08-17 01:45:04 -0400

    update for test of intel_2020_mpt_O_develop with hash ESMF_8_2_0_beta_snapshot_15 on discover [ci skip]

--- develop/discover/intel/2020/O/intelmpi/19.1.3.304/lib/esmf.mk
+++ develop/discover/intel/2020/O/intelmpi/19.1.3.304/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-16 01:19:42.594551
+build time -- 2021-08-17 01:09:10.161305
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/discover/intel/2020/O/mpt/2.17/lib/esmf.mk
+++ develop/discover/intel/2020/O/mpt/2.17/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-08-16 01:18:49.232730
+build time -- 2021-08-17 01:16:06.099278
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
