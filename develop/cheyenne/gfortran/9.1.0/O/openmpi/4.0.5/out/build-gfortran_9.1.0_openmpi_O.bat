5c0c4d21e9714ed2f1c7857e7e0c7b976c994d88
Mark Potts <mark.potts@noaa.gov>
2021-09-03 04:26:40 -0600

    update for test of gfortran_9.1.0_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/build-gfortran_9.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/build-gfortran_9.1.0_openmpi_O.bat
@@ -1,4 +1,30 @@
-Fri Sep 3 03:48:29 MDT 2021
+Fri Sep 3 04:26:20 MDT 2021
+#!/bin/sh -l
+#PBS -N build-gfortran_9.1.0_openmpi_O.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop
+module load gnu/9.1.0 openmpi/4.0.5 netcdf/4.7.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop/getres-build.sh
 #!/bin/sh -l
 #PBS -N build-gfortran_9.1.0_openmpi_O.bat
 #PBS -l walltime=1:00:00
