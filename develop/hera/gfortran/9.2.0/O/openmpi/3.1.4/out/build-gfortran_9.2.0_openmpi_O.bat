006df9db286c3880981c291c55e6a9ea8cc4e9a1
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-08 07:01:19 +0000

    update for test of intel_18.0.4_mpiuni_O_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Sep 8 06:16:42 UTC 2021
+Wed Sep 8 07:00:29 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_9.2.0_openmpi_O.bat_%j.o
--- develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Sep 8 06:16:41 UTC 2021
+Wed Sep 8 07:00:29 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_9.2.0_openmpi_O.bat_%j.o
