4c0433bff598dec2d748276067c6022df4cd372c
Mark Potts <mpotts@redlineperf.com>
2021-09-09 04:35:24 -0500

    update for test of intel_2020_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on orion [ci skip]

--- develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Sep 9 02:35:01 CDT 2021
+Thu Sep 9 04:34:42 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
--- develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Sep 9 02:35:01 CDT 2021
+Thu Sep 9 04:34:43 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_2020_intelmpi_g.bat_%j.o
