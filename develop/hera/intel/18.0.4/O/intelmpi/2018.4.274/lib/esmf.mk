27803a8ca265f02bda2a2f6ec9168c9f6bdc47be
Mark Potts <Mark.Potts@noaa.gov>
2021-09-10 03:30:07 -0400

    update for test of intel_18.0.4_intelmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on hera [ci skip]

--- develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/lib/esmf.mk
+++ develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-09-09 06:05:13
+build time -- 2021-09-10 06:26:05
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/intel/18.0.4/g/intelmpi/2018.4.274/lib/esmf.mk
+++ develop/hera/intel/18.0.4/g/intelmpi/2018.4.274/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-09-09 06:46:13
+build time -- 2021-09-10 06:22:32
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
