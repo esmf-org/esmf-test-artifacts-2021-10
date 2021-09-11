d92af52a170281e8f8add01b095df0571469d6bd
Mark Potts <Mark.Potts@noaa.gov>
2021-09-11 02:15:11 -0400

    update for test of intel_2020.2_intelmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on jet [ci skip]

--- develop/jet/intel/2020.2/g/intelmpi/2020.2/lib/esmf.mk
+++ develop/jet/intel/2020.2/g/intelmpi/2020.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-09-10 04:00:33
+build time -- 2021-09-11 04:00:36
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/jet/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
+++ develop/jet/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Sep 11 04:01:01 GMT 2021
+Sat Sep 11 06:01:26 GMT 2021
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_intelmpi_g.bat_%j.o
