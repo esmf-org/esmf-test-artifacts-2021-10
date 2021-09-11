5d5695c38bc574d2dcabb21060e6ecdc7f11359c
Mark Potts <Mark.Potts@noaa.gov>
2021-09-11 02:15:08 -0400

    update for test of gfortran_8.3.0_mpi_g_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on gaea [ci skip]

--- develop/gaea/gfortran/8.3.0/g/mpi/7.7.11/lib/esmf.mk
+++ develop/gaea/gfortran/8.3.0/g/mpi/7.7.11/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-09-10 00:26:05
+build time -- 2021-09-11 00:30:55
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/gaea/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
+++ develop/gaea/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Sep 11 00:25:06 EDT 2021
+Sat Sep 11 02:02:17 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
