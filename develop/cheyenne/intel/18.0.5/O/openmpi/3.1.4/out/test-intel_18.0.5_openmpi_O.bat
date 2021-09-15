8ec56fb3a171d3c5377ecfcc716f8873d9d868de
Mark Potts <mark.potts@noaa.gov>
2021-09-15 04:10:30 -0600

    update for build of intel_18.0.5_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_18-42-gb11ffe5 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Tue Sep 14 18:39:40 MDT 2021
+Wed Sep 15 04:10:30 MDT 2021
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=2:00:00
@@ -22,14 +22,14 @@ export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
-#make all_tests 2>&1| tee test_$JOBID.log 
-ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_openmpi_O_develop/getres-int.sh
+make all_tests 2>&1| tee test_$JOBID.log 
+ssh cheyenne6 /glade/scratch/mpotts//glade/scratch/mpotts/intel_18.0.5_openmpi_O_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_openmpi_O_develop/getres-int.sh
+ssh cheyenne6 /glade/scratch/mpotts//glade/scratch/mpotts/intel_18.0.5_openmpi_O_develop/getres-int.sh
 
 cd ../src/addon/ESMPy
 
