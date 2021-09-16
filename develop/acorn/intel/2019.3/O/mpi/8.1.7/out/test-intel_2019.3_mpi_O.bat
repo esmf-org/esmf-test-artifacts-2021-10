774a3e2f6ec5cd2d800990d99c268ef24f47c8cf
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-16 13:02:51 +0000

    update for build of intel_2019.3_mpi_O_develop with hash ESMF_8_2_0_beta_snapshot_19 on acorn [ci skip]

--- develop/acorn/intel/2019.3/O/mpi/8.1.7/out/test-intel_2019.3_mpi_O.bat
+++ develop/acorn/intel/2019.3/O/mpi/8.1.7/out/test-intel_2019.3_mpi_O.bat
@@ -1,4 +1,4 @@
-Thu Sep 16 12:33:29 UTC 2021
+Thu Sep 16 13:02:50 UTC 2021
 #!/bin/sh -l
 #PBS -N test-intel_2019.3_mpi_O.bat
 #PBS -l walltime=1:00:00
@@ -53,5 +53,3 @@ ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_O_develop/runpython.sh
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_O_develop/getres-test.sh
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_O_develop/getres-test.sh
