67363d72b72bd18c7a3af77ee5f10b530be2c9e3
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-16 13:03:24 +0000

    update for build of intel_2019.3_mpi_g_develop with hash ESMF_8_2_0_beta_snapshot_19 on acorn [ci skip]

--- develop/acorn/intel/2019.3/g/mpi/8.1.7/out/build-intel_2019.3_mpi_g.bat
+++ develop/acorn/intel/2019.3/g/mpi/8.1.7/out/build-intel_2019.3_mpi_g.bat
@@ -1,4 +1,4 @@
-Thu Sep 16 12:34:11 UTC 2021
+Thu Sep 16 13:03:23 UTC 2021
 #!/bin/sh -l
 #PBS -N build-intel_2019.3_mpi_g.bat
 #PBS -l walltime=1:00:00
@@ -39,4 +39,3 @@ export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make -j 128 2>&1| tee build_$JOBID.log
 
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_g_develop/getres-build.sh
