f5a657d3307ba86532cc7c93aad2fa9310701e64
Mark Potts <Mark.Potts@noaa.gov>
2021-09-20 10:15:06 -0400

    update for build of gfortran_8.3.0_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_19-1-g109b94b on orion [ci skip]

--- develop/orion/gfortran/8.3.0/O/mpiuni/out/build-gfortran_8.3.0_mpiuni_O.bat
+++ develop/orion/gfortran/8.3.0/O/mpiuni/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Sat Sep 18 14:14:31 CDT 2021
+Mon Sep 20 08:59:52 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_mpiuni_O.bat_%j.o
--- develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Sep 18 17:30:57 CDT 2021
+Mon Sep 20 08:59:58 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_2020_intelmpi_g.bat_%j.o
@@ -27,11 +27,13 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
+ssh Orion-login-1.HPC.MsState.Edu /work/noaa/da/mpotts/sandbox//work/noaa/da/mpotts/sandbox/intel_2020_intelmpi_g_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
+ssh Orion-login-1.HPC.MsState.Edu /work/noaa/da/mpotts/sandbox//work/noaa/da/mpotts/sandbox/intel_2020_intelmpi_g_develop/getres-int.sh
 
 cd ../src/addon/ESMPy
 
