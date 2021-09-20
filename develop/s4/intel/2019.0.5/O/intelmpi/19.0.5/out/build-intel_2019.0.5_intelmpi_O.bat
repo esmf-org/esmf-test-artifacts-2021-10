32b82b6bfc19fbe807e44210084f7ef1d8ae0048
Mark Potts <mark.potts@noaa.gov>
2021-09-20 14:23:20 +0000

    update for build of intel_2019.0.5_intelmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_10 on s4 [ci skip]

--- develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/build-intel_2019.0.5_intelmpi_O.bat
+++ develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/build-intel_2019.0.5_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Mon Sep 20 14:22:45 UTC 2021
+Mon Sep 20 14:23:17 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=star
 #SBATCH -o build-intel_2019.0.5_intelmpi_O.bat_%j.o
@@ -12,7 +12,6 @@ Mon Sep 20 14:22:45 UTC 2021
 export JOBID=$SLURM_JOBID
 
 module load license_intel/S4
-export ESMF_MPIRUN=mpirun.srun
 module load intel/19.0.5 hdf hdf5 impi/19.0.5 netcdf4/4.7.3
 module load hdf5/1.10.5 
 module list >& module-build.log
@@ -20,7 +19,7 @@ module list >& module-build.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/scratch/users/mpotts/intel_2019.0.5_intelmpi_O_develop
+export ESMF_DIR=/data/users/mpotts/intel_2019.0.5_intelmpi_O_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=intelmpi
 export ESMF_BOPT='O'
