add45c406077125a7cd07d5d4c9f1daa32dfc841
Mark Potts <mark.potts@noaa.gov>
2021-09-20 14:28:31 +0000

    update for test of intel_2019.0.5_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_19-1-g109b94b on s4 [ci skip]

--- develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_g.bat
+++ develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon Sep 20 14:22:52 UTC 2021
+Mon Sep 20 14:28:29 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=star
 #SBATCH -o test-intel_2019.0.5_intelmpi_g.bat_%j.o
@@ -12,6 +12,7 @@ Mon Sep 20 14:22:52 UTC 2021
 export JOBID=$SLURM_JOBID
 
 module load license_intel/S4
+export ESMF_MPIRUN=mpirun.srun
 module load intel/19.0.5 hdf hdf5 impi/19.0.5 netcdf4/4.7.3
 module load hdf5/1.10.5 
 module list >& module-test.log
@@ -19,7 +20,7 @@ module list >& module-test.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/data/users/mpotts/intel_2019.0.5_intelmpi_g_develop
+export ESMF_DIR=/scratch/users/mpotts/intel_2019.0.5_intelmpi_g_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=intelmpi
 export ESMF_BOPT='g'
@@ -28,3 +29,10 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
+ssh s4-submit.ssec.wisc.edu /scratch/users/mpotts//scratch/users/mpotts/intel_2019.0.5_intelmpi_g_develop/getres-int.sh
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
+ssh s4-submit.ssec.wisc.edu /scratch/users/mpotts//scratch/users/mpotts/intel_2019.0.5_intelmpi_g_develop/getres-int.sh
