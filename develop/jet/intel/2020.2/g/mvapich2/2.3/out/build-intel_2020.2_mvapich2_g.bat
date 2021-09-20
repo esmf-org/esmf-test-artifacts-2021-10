a1ee9a74b125a8e6e05b45eb1db02f8a13212175
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-20 12:37:02 +0000

    update for test of intel_2020.2_mvapich2_g_develop with hash ESMF_8_2_0_beta_snapshot_19-1-g109b94b on jet [ci skip]

--- develop/jet/intel/2020.2/g/mvapich2/2.3/out/build-intel_2020.2_mvapich2_g.bat
+++ develop/jet/intel/2020.2/g/mvapich2/2.3/out/build-intel_2020.2_mvapich2_g.bat
@@ -1,4 +1,4 @@
-Mon Sep 20 12:26:27 GMT 2021
+Mon Sep 20 12:37:00 GMT 2021
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o build-intel_2020.2_mvapich2_g.bat_%j.o
--- develop/jet/intel/2020.2/g/mvapich2/2.3/out/test-intel_2020.2_mvapich2_g.bat
+++ develop/jet/intel/2020.2/g/mvapich2/2.3/out/test-intel_2020.2_mvapich2_g.bat
@@ -1,4 +1,4 @@
-Mon Sep 20 12:26:27 GMT 2021
+Mon Sep 20 12:36:59 GMT 2021
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_mvapich2_g.bat_%j.o
