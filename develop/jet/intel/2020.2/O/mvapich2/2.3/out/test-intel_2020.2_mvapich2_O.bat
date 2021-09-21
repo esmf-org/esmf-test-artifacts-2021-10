0863b952e155f0007c9a8628c99a88b0503234db
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-21 04:11:49 +0000

    update for build of intel_2020.2_mvapich2_O_develop with hash ESMF_8_2_0_beta_snapshot_19-2-ge6c8eaf on jet [ci skip]

--- develop/jet/intel/2020.2/O/mvapich2/2.3/out/test-intel_2020.2_mvapich2_O.bat
+++ develop/jet/intel/2020.2/O/mvapich2/2.3/out/test-intel_2020.2_mvapich2_O.bat
@@ -1,4 +1,4 @@
-Mon Sep 20 12:40:51 GMT 2021
+Tue Sep 21 04:11:46 GMT 2021
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_mvapich2_O.bat_%j.o
@@ -30,10 +30,10 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-ssh fe2 /mnt/lfs4/HFIP/hfv3gfs/Mark.Potts//mnt/lfs4/HFIP/hfv3gfs/Mark.Potts/intel_2020.2_mvapich2_O_develop/getres-int.sh
+ssh fe3 /mnt/lfs4/HFIP/hfv3gfs/Mark.Potts//mnt/lfs4/HFIP/hfv3gfs/Mark.Potts/intel_2020.2_mvapich2_O_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh fe2 /mnt/lfs4/HFIP/hfv3gfs/Mark.Potts//mnt/lfs4/HFIP/hfv3gfs/Mark.Potts/intel_2020.2_mvapich2_O_develop/getres-int.sh
+ssh fe3 /mnt/lfs4/HFIP/hfv3gfs/Mark.Potts//mnt/lfs4/HFIP/hfv3gfs/Mark.Potts/intel_2020.2_mvapich2_O_develop/getres-int.sh
