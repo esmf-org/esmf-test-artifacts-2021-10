d0adbf206b8cbf48f577e31c4bb386867a51935d
Mark Potts <Mark.Potts@noaa.gov>
2021-09-22 01:30:05 -0400

    update for test of intel_2019.5_mpiuni_O_develop with hash ESMF_8_2_0_beta_snapshot_19-3-ga5c5083 on gaea [ci skip]

--- develop/gaea/intel/2019.5/O/mpiuni/lib/esmf.mk
+++ develop/gaea/intel/2019.5/O/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-09-21 00:31:40
+build time -- 2021-09-22 00:27:32
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
@@ -86,6 +86,7 @@ ESMF_TRACE_STATICLINKLIBS=-lesmftrace_static
 # Internal ESMF variables, do NOT depend on these!
 
 ESMF_INTERNAL_DIR=/lustre/f2/dev/ncep/Mark.Potts/intel_2019.5_mpiuni_O_develop
+ESMF_INTERNAL_MPIRUN=/lustre/f2/dev/ncep/Mark.Potts/intel_2019.5_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
 
 #
 # !!! The following options were used on this ESMF build !!!
