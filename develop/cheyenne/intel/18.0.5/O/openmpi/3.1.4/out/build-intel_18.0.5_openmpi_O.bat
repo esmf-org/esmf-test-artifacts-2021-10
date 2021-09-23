f70ce1959d14f4e3290ec96e5cfe01595384bc6e
Mark Potts <mark.potts@noaa.gov>
2021-09-23 04:08:30 -0600

    update for build of intel_18.0.5_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_19-8-gf4df752 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
@@ -1,8 +1,8 @@
-Wed Sep 22 06:09:08 MDT 2021
+Thu Sep 23 04:08:21 MDT 2021
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=1:00:00
-#PBS -l walltime=2:00:00
+#PBS -l walltime=3:00:00
 #PBS -q regular
 #PBS -A p48503002
 #PBS -l select=1:ncpus=36:mpiprocs=36
--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
@@ -1,7 +1,7 @@
-Wed Sep 22 06:09:08 MDT 2021
+Thu Sep 23 04:08:21 MDT 2021
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_O.bat
-#PBS -l walltime=2:00:00
+#PBS -l walltime=3:00:00
 #PBS -q regular
 #PBS -A p48503002
 #PBS -l select=1:ncpus=36:mpiprocs=36
