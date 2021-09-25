91f8da86de5ba414d085951e8d8f9139a5542561
Mark Potts <Mark.Potts@noaa.gov>
2021-09-25 14:00:03 -0400

    update for build of gfortran_9.3.0_openmpi_O_develop with hash ESMF_8_2_0_beta_snapshot_19-13-g03d9101 on chianti [ci skip]

--- develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
+++ develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Sep 25 13:27:39 EDT 2021
+Sat Sep 25 13:45:33 EDT 2021
 #!/bin/bash -l
 export JOBID=12345
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
