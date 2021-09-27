8e007eadc23b685af962e14d72bb22b18acb8791
Mark Potts <Mark.Potts@noaa.gov>
2021-09-27 06:15:08 -0400

    update for test of gfortran_8.3.0_mpiuni_g_develop with hash ESMF_8_2_0_beta_snapshot_20 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Mon Sep 27 05:44:34 EDT 2021
+Mon Sep 27 06:12:11 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
--- develop/discover/pgi/2020/O/mpiuni/out/build-pgi_2020_mpiuni_O.bat
+++ develop/discover/pgi/2020/O/mpiuni/out/build-pgi_2020_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Mon Sep 27 04:55:17 EDT 2021
+Mon Sep 27 06:09:35 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_2020_mpiuni_O.bat_%j.o
