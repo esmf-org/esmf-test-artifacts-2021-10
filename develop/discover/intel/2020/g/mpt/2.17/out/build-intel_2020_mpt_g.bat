fb60a25a103471920984da5339605230f3f76147
Mark Potts <Mark.Potts@noaa.gov>
2021-09-27 05:15:03 -0400

    update for build of intel_2020_mpt_g_develop with hash ESMF_8_2_0_beta_snapshot_20 on discover [ci skip]

--- develop/discover/intel/2020/g/mpt/2.17/out/build-intel_2020_mpt_g.bat
+++ develop/discover/intel/2020/g/mpt/2.17/out/build-intel_2020_mpt_g.bat
@@ -1,4 +1,4 @@
-Sun Sep 26 01:40:23 EDT 2021
+Mon Sep 27 05:14:26 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_mpt_g.bat_%j.o
--- develop/discover/intel/2020/g/mpt/2.17/out/test-intel_2020_mpt_g.bat
+++ develop/discover/intel/2020/g/mpt/2.17/out/test-intel_2020_mpt_g.bat
@@ -1,4 +1,4 @@
-Sun Sep 26 01:40:23 EDT 2021
+Mon Sep 27 05:14:26 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_mpt_g.bat_%j.o
