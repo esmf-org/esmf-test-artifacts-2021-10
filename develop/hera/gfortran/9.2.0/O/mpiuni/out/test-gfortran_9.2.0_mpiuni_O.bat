6ae680bd2a423f6cf859aaeef741ca58bfbbe3c1
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-29 06:43:28 +0000

    update for build of gfortran_9.2.0b_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_20 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/O/mpiuni/out/test-gfortran_9.2.0_mpiuni_O.bat
+++ develop/hera/gfortran/9.2.0/O/mpiuni/out/test-gfortran_9.2.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Wed Sep 29 05:59:59 UTC 2021
+Wed Sep 29 06:18:34 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_9.2.0_mpiuni_O.bat_%j.o
--- develop/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Tue Sep 28 06:43:42 UTC 2021
+Wed Sep 29 06:42:46 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_9.2.0_mpiuni_g.bat_%j.o
