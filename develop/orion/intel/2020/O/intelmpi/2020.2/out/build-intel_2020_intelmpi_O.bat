14bac5af77f3a73694ee4499d9aaec19eba51916
Mark Potts <Mark.Potts@noaa.gov>
2021-09-29 07:45:07 -0400

    update for test of intel_2020_intelmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_20 on orion [ci skip]

--- develop/orion/intel/2020/O/intelmpi/2020.2/out/build-intel_2020_intelmpi_O.bat
+++ develop/orion/intel/2020/O/intelmpi/2020.2/out/build-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Sep 29 02:33:47 CDT 2021
+Wed Sep 29 06:38:46 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_2020_intelmpi_O.bat_%j.o
--- develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Sep 29 02:32:16 CDT 2021
+Wed Sep 29 06:38:41 CDT 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
